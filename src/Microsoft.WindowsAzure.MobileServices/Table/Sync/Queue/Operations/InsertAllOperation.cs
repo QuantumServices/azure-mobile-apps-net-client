a9f1f81c267fc35a4130e353b3f929025375fb55
shirazamin <shirazam@buffalo.edu>
2017-03-28 15:23:31 -0400

    Added the bulk insert operation and a simple test for it

---
+++ src/Microsoft.WindowsAzure.MobileServices/Table/Sync/Queue/Operations/InsertAllOperation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.WindowsAzure.MobileServices.Sync
+{
+    internal class InsertAllOperation : MobileServiceTableBulkOperation
+    {
+        public InsertAllOperation(string tableName, MobileServiceTableKind tableKind, IEnumerable<string> itemIds)
+            : base(tableName, tableKind, itemIds)
+        {
+            this.Operations = itemIds.Select(itemId => new InsertOperation(tableName, tableKind, itemId));
+        }
+
+        public override MobileServiceTableOperationKind Kind
+        {
+            get
+            {
+                return MobileServiceTableOperationKind.Insert;
+            }
+        }
+
+        public override async Task ExecuteLocalAsync(IMobileServiceLocalStore store, IEnumerable<JObject> items)
+        {
+            var currentItems = await store.LookupAsync(this.TableName, this.ItemIds);
+
+            if (currentItems != null && currentItems.Any())
+            {
+                throw new MobileServiceLocalStoreException("An insert operation on one or more of these items is already in the queue.", null);
+            }
+            await store.UpsertAsync(this.TableName, items, fromServer: false);
+        }
+
+        protected override Task<IEnumerable<JToken>> OnExecuteAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ unittest/Microsoft.WindowsAzure.MobileServices.Test.Unit/Table/Sync/Queue/Operations/InsertAllOperationTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Moq;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.WindowsAzure.MobileServices.Test.Unit.Table.Sync.Queue.Operations
+{
+    [TestClass]
+    public class InsertAllOperationTests
+    {
+        private InsertAllOperation operation;
+        private long bulkInsertCount = 1000;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            IList<string> ids = new List<string>();
+            for (int i = 0; i < bulkInsertCount; i++)
+            {
+                ids.Add("abc" + i);
+            }
+            this.operation = new InsertAllOperation("test", MobileServiceTableKind.Table, ids);
+        }
+
+        [TestMethod]
+        public async Task ExecuteLocalAsync_UpsertsItemsOnStore()
+        {
+            var store = new Mock<IMobileServiceLocalStore>(MockBehavior.Loose);
+            IList<JObject> items = new List<JObject>();
+            for (long i = 0; i < bulkInsertCount; i++)
+            {
+                var item = JObject.Parse("{\"id\":\"abc" + i + "\",\"Text\":\"Example\"}");
+                items.Add(item);
+            }
+            await this.operation.ExecuteLocalAsync(store.Object, items);
+            var Item = items.FirstOrDefault();
+            store.Verify(s => s.UpsertAsync("test", It.IsIn<IEnumerable<JObject>>(items), false), Times.Once());
+        }
+    }
+}
